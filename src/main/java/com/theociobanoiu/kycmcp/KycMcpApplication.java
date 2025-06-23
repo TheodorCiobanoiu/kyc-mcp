208d584010b34b64216cda6b09dcdaf441b120ec
TheoCiobanoiu <theodor.ciobanoiu@gmail.com>
2025-06-23 21:35:42 +0300

    Initial Commit

---
+++ src/main/java/com/theociobanoiu/kycmcp/KycMcpApplication.java
@@ -0,0 +1,13 @@
+package com.theociobanoiu.kycmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KycMcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theociobanoiu/kycmcp/KycMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theociobanoiu.kycmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
