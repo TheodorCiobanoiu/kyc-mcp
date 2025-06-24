99677541782fb23c4c7aed423b747673e1a2b526
TheoCiobanoiu <theodor.ciobanoiu@gmail.com>
2025-06-24 21:30:37 +0300

    Added JPA repositories for Client and Person entities

--- src/main/java/com/theociobanoiu/kycmcp/repository/ClientRepository.java
+++ src/main/java/com/theociobanoiu/kycmcp/repository/ClientRepository.java
@@ -1,8 +1,32 @@
 package com.theociobanoiu.kycmcp.repository;
 
 import com.theociobanoiu.kycmcp.model.entities.Client;
+import com.theociobanoiu.kycmcp.model.enums.ClientType;
+import com.theociobanoiu.kycmcp.model.enums.RiskLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-interface ClientRepository extends JpaRepository<Client, Long> {
+import java.util.List;
 
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findByNameContainingIgnoreCase(String name);
+
+    List<Client> findByClientType(ClientType clientType);
+
+    List<Client> findByRiskLevel(RiskLevel riskLevel);
+
+    List<Client> findByRiskLevelOrderByNameAsc(RiskLevel riskLevel);
+
+    @Query("""
+        SELECT c FROM Client c WHERE
+        (:name IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))) AND 
+        (:clientType IS NULL OR c.clientType = :clientType) AND 
+        (:riskLevel IS NULL OR c.riskLevel = :riskLevel)""")
+    List<Client> searchClients(@Param("name") String name,
+        @Param("clientType") ClientType clientType,
+        @Param("riskLevel") RiskLevel riskLevel);
 }
--- src/main/java/com/theociobanoiu/kycmcp/repository/PersonRepository.java
+++ src/main/java/com/theociobanoiu/kycmcp/repository/PersonRepository.java
@@ -1,8 +1,26 @@
 package com.theociobanoiu.kycmcp.repository;
 
 import com.theociobanoiu.kycmcp.model.entities.Person;
+import com.theociobanoiu.kycmcp.model.enums.RelationshipType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-interface PersonRepository extends JpaRepository<Person, Long> {
+import java.util.List;
 
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByClientId(Long clientId);
+
+    List<Person> findByRelationshipType(RelationshipType relationshipType);
+
+    @Query("""
+        SELECT p FROM Person p WHERE
+        LOWER(CONCAT(p.firstName, ' ', p.lastName)) LIKE LOWER(CONCAT('%', :fullName, '%'))""")
+    List<Person> findByFullNameContaining(@Param("fullName") String fullName);
+
+    @Query("SELECT p FROM Person p WHERE p.relationshipType = 'BENEFICIAL_OWNER'")
+    List<Person> findAllBeneficialOwners();
 }
